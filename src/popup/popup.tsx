81817cb51fa6e26994dc08060d3642cf1f2bc0e6
MauricioMSoares <mauricio.soares@sptech.school>
2023-11-29 15:42:22 -0300

    feat: added chrome storage usage example

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { createRoot } from 'react-dom/client'
-import '../assets/tailwind.css'
+import './popup.css'
 
-const Popup = () => (
-    <>
-        <div>
-            <img src="icon.png" alt="" />
-            <h1 className="text-3xl text-black-500">Hello World</h1>
-        </div>
-    </>
-)
+const handleInput = (e) => {
+    const name = e.target[0].value
+    chrome.storage.sync.set({ name: name }, () => {
+        console.log("Name is set to " + name)
+    })
+}
+
+const Popup = () => {
+
+    useEffect(() => {
+        chrome.storage.sync.get(["name"], (res) => {
+            console.log(res.name)
+        })
+    }, [])
+
+    return (
+        <>
+            <div className="h-screen">
+                <form onSubmit={handleInput} className="flex justify-center items-center py-44">
+                    <input type="text" name="name" className="bg-gray ring-black px-4 py-4" placeholder="Enter a word" />
+                    <button className="py-4 px-3 bg-indigo-500 text-white m-2">Submit</button>
+                </form>
+            </div>
+        </>
+    )
+}
 
 export default Popup
